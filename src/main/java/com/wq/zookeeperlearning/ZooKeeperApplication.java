fdba74281065cc53442f21fa5ea679ec854bc005
KingS <770234358@qq.com>
2021-07-01 19:16:26 +0800

    运行ZooKeeperApplication。 运行SpringBoot默认需要配置数据源

---
+++ src/main/java/com/wq/zookeeperlearning/ZooKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.wq.zookeeperlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooKeeperApplication {
+    public static void main(String[] args) {
+        // 问题1.需要配置数据源
+        SpringApplication.run(ZooKeeperApplication.class);
+    }
+}
